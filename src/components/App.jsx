c4963d0b7eda8250f46ab0c28079437495db49cc
Yevhen Podoliaka <zanp85740@gmail.com>
2022-07-23 18:54:37 +0300

    remove styles component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,37 @@
-import React, { Component } from 'react'
-import Searchbar  from './Searchbar/Searchbar'
+import React, { Component } from 'react';
+import Searchbar from './Searchbar/Searchbar';
+import styled from 'styled-components';
+
 
 export class App extends Component {
   state = {
-    query:''
-  }
-  onSubmit = (value) => {
-    this.setState({ query: value })
+    query: '',
+    page: 1,
+    items:[],
+  };
+  componentDidMount() {  }
+  componentDidUpdate(prevProps, prevState) {} 
+onSubmit = value => {
+    this.setState({ query: value });
     console.log(value);
-  }
-  render() {
-    return <Searchbar onSubmit={this.onSubmit} />
-  }
+   
+  };
 
-};
-/*
- * Стилі компонента App
- */
-// .App {
-//   display: grid;
-//   grid-template-columns: 1fr;
-//   grid-gap: 16px;
-//   padding-bottom: 24px;
-// }
-// const Wrap = styled.div`
-//   display: grid;
-//   grid-template-columns: 1fr;
-//   grid-gap: 16px;
-//   padding-bottom: 24px;`
+  render() {
+    return (
+      <Container>
+        <Searchbar onSubmit={this.onSubmit} />
+      </Container>
+    );
+  }
+}
+
+
+const Container = styled.div`
+   display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 16px;
+  padding-bottom: 24px;`
+
+
+
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
-
+import { Gallery } from './ImageGallery.styled'
 export default class ImageGallery extends Component{
     state = {
     
     }
+  componentDidMount() {  }
+  componentDidUpdate(prevProps, prevState) {} 
     render() {
         return <Gallery >
             
@@ -12,14 +13,4 @@ export default class ImageGallery extends Component{
     }
 }
 
-const Gallery = styled.ul`
-  display: grid;
-  max-width: calc(100vw - 48px);
-  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
-  grid-gap: 16px;
-  margin-top: 0;
-  margin-bottom: 0;
-  padding: 0;
-  list-style: none;
-  margin-left: auto;
-  margin-right: auto;`
+
